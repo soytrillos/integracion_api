dc7e0437bdc9d1bbe8f31d1b76ae96b5f53186ce
strillosbex <simon.trillos@bexsoluciones.com>
2022-05-24 15:34:00 -0500

    v1.2.6 release 2.6

--- routes/v2/router_images.py
+++ routes/v2/router_images.py
@@ -26,8 +26,8 @@ async def upload_file(files: List[UploadFile]):
 async def files_m(empresa: str = Form(...), imagenes: List[UploadFile] = File(...)):
     resultado = {}
     resultado = []
-    ruta_archivo_temporal = f"{os.getcwd()}\\server\\images\\{empresa}_temp\\"
-    ruta_archivo = f"{os.getcwd()}\\server\\images\\{empresa}\\"
+    ruta_archivo_temporal = fr"{os.getcwd()}\server\images\{empresa}_temp"
+    ruta_archivo = fr"{os.getcwd()}\server\images\{empresa}"
     if os.path.exists(ruta_archivo) == False:
         os.mkdir(ruta_archivo)
     elif os.path.exists(ruta_archivo_temporal) == False:
@@ -36,7 +36,7 @@ async def files_m(empresa: str = Form(...), imagenes: List[UploadFile] = File(..
     for imagen in imagenes:
         img = str(imagen.content_type)
         if img.count('image') == 1:
-            with open(f"{ruta_archivo_temporal}{imagen.filename}", "wb") as myfile:
+            with open(fr"{ruta_archivo_temporal}\{imagen.filename}", "wb") as myfile:
                 content = await imagen.read()
                 myfile.write(content)
                 myfile.close()
@@ -50,8 +50,8 @@ async def files_m(empresa: str = Form(...), imagenes: List[UploadFile] = File(..
 
 @router.get("/search_images/{empresa}/{imagen}", tags=["Carga Datos"])
 async def s_images(empresa: str, imagen: str):
-    busqueda = os.path.isfile(f"{os.getcwd()}\\server\\images\\{empresa}\\{imagen}")
-    file_img = f"{os.getcwd()}\\server\\images\\{empresa}\\{imagen}"
+    busqueda = os.path.isfile(fr"{os.getcwd()}\server\images\{empresa}\{imagen}")
+    file_img = fr"{os.getcwd()}\server\\images\{empresa}\{imagen}"
     if busqueda == True:
         return FileResponse(path=file_img)
     else:
--- services/v2/imagenes_wms.py
+++ services/v2/imagenes_wms.py
@@ -10,9 +10,9 @@ class maestra_imagenes:
         self.ruta_img_def = ruta_img_def
         
     def cambio_extension(self):
-        img_origen = f"{self.ruta_imagen}\\{self.imagen}"
+        img_origen = fr"{self.ruta_imagen}\{self.imagen}"
         img = Image.open(img_origen)
         name_image = str(self.imagen).replace('.jpg', '').replace('.png', '').replace('.gif', '').replace('.svg', '').replace('.jpeg', '')
-        img.save(f'{self.ruta_img_def}{name_image}.jpg', quality=95)
+        img.save(fr'{self.ruta_img_def}\{name_image}.jpg', quality=95)
         os.remove(img_origen)
         return name_image
